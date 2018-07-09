d838cb3a337b7165d06de58e056149e8d4cdc006
Maryna Gryshko <chumakova.marina@gmail.com>
2018-07-09 12:47:25 +0300

    First edition

--- SensorTagWeb/ViewController.swift
+++ SensorTagWeb/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadWebView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func loadWebView()
+    {
+        //let url = URL(string: "https://medium.com/@felicity.johnson.mail/web-view-tutorial-swift-3-0-4a5f4f6858d3")
+        let url = URL(string: "http://iotamaze.io")
+        if let unwrappedURL = url {
+            let request = URLRequest(url: unwrappedURL)
+            let session = URLSession.shared
+            
+            let task = session.dataTask(with: request) { (data, response, error) in
+                if error == nil
+                {
+                    DispatchQueue.main.async {
+                       self.webView.load(request)
+                    }
+                } else
+                {
+                    print("ERROR:\(error)")
+                }
+            }
+            task.resume()
+     }
+    }
 
 }
 
